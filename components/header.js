eb87c06ee31dbe4d823e8296402c59ef4570e2fe
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2023-07-14 17:00:10 +0700

    10th commit

--- components/header.js
+++ components/header.js
@@ -1,10 +1,17 @@
 import Link from 'next/link'
+import Typewriter from 'typewriter-effect';
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-20">
       <Link href="/" className="hover:underline">
-        Wiwit. | Jelajah Ensiklopedia Alam
+                <Typewriter
+                  options={{
+                    strings: ['Studiofru | Green Project','Jelajah Ensiklopedia', 'Flora'],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
       </Link>
     </h2>
   )
