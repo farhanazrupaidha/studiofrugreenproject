4271fcb82ad4c867ade553304f4affd15f2761c0
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2023-11-19 17:06:46 +0700

    43th commit

--- pages/_document.js
+++ pages/_document.js
@@ -41,15 +41,19 @@ export default class MyDocument extends Document {
             }} />
             {/* google adsense */}
             {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9GSS5SE4QY" />
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
             <Script
                 dangerouslySetInnerHTML={{
                 __html: `
-                window.dataLayer = window.dataLayer || [];
+                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-              
-                gtag('config', 'G-9GSS5SE4QY');
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
                 `,
                 }}
             />
