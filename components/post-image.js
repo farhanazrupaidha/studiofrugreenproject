0c102d4542aff78cf52401c07b317a533e8b2593
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2023-12-20 10:16:10 +0700

    Update post-image.js

--- components/post-image.js
+++ components/post-image.js
@@ -90,8 +90,8 @@ export default function PostImage({ cloudinaryImageLibrary, title }) {
           <SplideSlide>
             <Box sx={{ m:'auto', textAlign: 'center' }}>
               <CldImage
-                width={pic.width/2}
-                height={pic.height/2}
+                width={pic.width/1.5}
+                height={pic.height/1.5}
                 src={pic.public_id}
                 alt={title}
                 blurDataURL={pic.public_id}
