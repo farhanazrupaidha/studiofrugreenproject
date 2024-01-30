306d77a69bc5548f14444c8f453f813b16f1aa1f
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-01-30 16:34:05 +0700

    57th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -129,7 +129,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                        
+    </url>  
+    <url>
+        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-pacing-cheilocostus-speciosus</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                            
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://www.studiofrugreenproject.com/en/posts/${slug}</loc>
