8d1666376fd2d5f02bec212244bf1d2341aae5df
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-02-27 16:58:58 +0700

    62nd commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -135,7 +135,25 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                      
+    </url>      
+    <url>
+        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-ginje-cascabela-thevetia-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>         
+    <url>
+        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-teki-cyperus-rotundus-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>  
+    <url>
+        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bunga-pukul-empat-mirabilis-jalapa-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                        
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://www.studiofrugreenproject.com/en/posts/${slug}</loc>
