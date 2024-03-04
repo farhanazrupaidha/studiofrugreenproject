b5801ea2f803af6aa172d75259b7bf657122c84d
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-03-04 10:58:30 +0700

    68th commit
    change to head domain

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -8,44 +8,44 @@ export const getServerSideProps = async ({ res }) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://www.studiofrugreenproject.com</loc>
+        <loc>https://studiofrugreenproject.com</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>
     <url>
-        <loc>https://www.studiofrugreenproject.com/en</loc>
+        <loc>https://studiofrugreenproject.com/en</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>
     <url>
-        <loc>https://www.studiofrugreenproject.com/flora</loc>
+        <loc>https://studiofrugreenproject.com/flora</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/fauna</loc>
+        <loc>https://studiofrugreenproject.com/fauna</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>   
     <url>
-        <loc>https://www.studiofrugreenproject.com/en/flora</loc>
+        <loc>https://studiofrugreenproject.com/en/flora</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/en/fauna</loc>
+        <loc>https://studiofrugreenproject.com/en/fauna</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.5</priority>
     </url>     
     ${postsId.map(({ slug, updatedAt }) => `
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/${slug}</loc>
+        <loc>https://studiofrugreenproject.com/posts/${slug}</loc>
         <lastmod>${updatedAt}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
@@ -53,122 +53,122 @@ export const getServerSideProps = async ({ res }) => {
     `)  
     .join('\n')}    
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-sirih-gading-epipremnum-aureum-bagi-lingkungan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-sirih-gading-epipremnum-aureum-bagi-lingkungan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url> 
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bunga-bakung-hymenocallis-littoralis-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bunga-bakung-hymenocallis-littoralis-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/apa-itu-ekoenzim-dan-bagaimana-cara-membuatnya</loc>
+        <loc>https://studiofrugreenproject.com/posts/apa-itu-ekoenzim-dan-bagaimana-cara-membuatnya</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>   
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/asal-penyebaran-dan-ancaman-ulat-ngengat-hippotion-celerio-sebagai-hama</loc>
+        <loc>https://studiofrugreenproject.com/posts/asal-penyebaran-dan-ancaman-ulat-ngengat-hippotion-celerio-sebagai-hama</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-miana-plectranthus-scutellarioides-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-miana-plectranthus-scutellarioides-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>   
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-manfaat-bunga-bakung-merah-amaryllis-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-manfaat-bunga-bakung-merah-amaryllis-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>     
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-kegunaan-sunset-bells-chrysothemis-pulchella</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-kegunaan-sunset-bells-chrysothemis-pulchella</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url> 
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-manfaat-mondokaki-tabernaemontana-divaricata-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-efek-dan-manfaat-mondokaki-tabernaemontana-divaricata-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bungur-kecil-lagerstroemia-indica-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bungur-kecil-lagerstroemia-indica-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-kangkung-air-ipomoea-aquatica-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-kangkung-air-ipomoea-aquatica-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>   
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-lili-perdamaian-spathiphyllum-kochii-bagi-lingkungan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-lili-perdamaian-spathiphyllum-kochii-bagi-lingkungan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url> 
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/karakter-tanah-yang-baik-bagi-pertanian</loc>
+        <loc>https://studiofrugreenproject.com/posts/karakter-tanah-yang-baik-bagi-pertanian</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url> 
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-pacing-cheilocostus-speciosus</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-pacing-cheilocostus-speciosus</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url> 
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/dampak-terhadap-lingkungan-dan-potensi-lain-dari-galian-c-kopi-luhur-cirebon</loc>
+        <loc>https://studiofrugreenproject.com/posts/dampak-terhadap-lingkungan-dan-potensi-lain-dari-galian-c-kopi-luhur-cirebon</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>      
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-ginje-cascabela-thevetia-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-ginje-cascabela-thevetia-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>         
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-teki-cyperus-rotundus-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-teki-cyperus-rotundus-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>  
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bunga-pukul-empat-mirabilis-jalapa-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-bunga-pukul-empat-mirabilis-jalapa-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-soka-ixora-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-soka-ixora-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>    
     <url>
-        <loc>https://www.studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-flamboyan-delonix-regia-bagi-kesehatan</loc>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-flamboyan-delonix-regia-bagi-kesehatan</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>                                                          
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
-        <loc>https://www.studiofrugreenproject.com/en/posts/${slug}</loc>
+        <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
         <lastmod>${updatedAt}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
