3bddaa18f2ba62913dcf0357cad8252662850250
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-04-02 15:15:45 +0700

    83rd commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -171,7 +171,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-girang-mali-mali-leea-indica-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                  
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
