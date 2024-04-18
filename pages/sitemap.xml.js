ecbd5dba03d72bb82f642d2e0905f653d686b292
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-04-18 15:50:12 +0700

    86th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -207,7 +207,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                        
+    </url>     
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-efek-racun-dan-manfaat-kamboja-jepang-adenium-obesum-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                     
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
