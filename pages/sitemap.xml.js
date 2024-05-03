6152994c7e6e9090b1f3a70d9cac5edcaae46e00
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-05-03 14:08:49 +0700

    88th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -237,7 +237,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                 
+    </url>    
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-kucing-galak-anting-anting-acalypha-indica-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                  
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
