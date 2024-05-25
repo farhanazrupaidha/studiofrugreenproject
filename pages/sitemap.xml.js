ba064cc7989b215b7a3a6c32502e41b192a7b8e6
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-05-25 14:51:31 +0700

    88th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -261,7 +261,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                          
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/deskripsi-asal-dan-manfaat-rumput-israel-asystasia-gangetica-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                             
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
