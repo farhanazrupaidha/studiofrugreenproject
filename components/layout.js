1b78a9f4ae14b7a2d6c486b48628a01defa02345
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-05-28 15:47:06 +0700

    90th commit
    beri dukungan button deleted

--- components/layout.js
+++ components/layout.js
@@ -3,33 +3,11 @@ import dynamic from 'next/dynamic'
 const ResponsiveAppBar = dynamic(() => import('../components/appbar'), { ssr: false })
 const Footer = dynamic(() => import('../components/footer'), { ssr: false })
 
-import Button from '@mui/material/Button';
-import RecommendIcon from '@mui/icons-material/Recommend';
-
 export default function Layout({ preview, children }) {
   return (
   <>
     <ResponsiveAppBar />
-    <div className="min-h-screen">
-    <Button 
-        variant="contained" 
-        color="secondary" 
-        size="large"
-        sx={{
-          borderRadius: 5, 
-          position: 'fixed', 
-          bottom: "50px", 
-          left:'50%', 
-          transform: 'translate(-50%, -50%)', 
-          display:'block', 
-          boxShadow: 100, 
-          fontWeight: 'bold'
-          }} 
-          href="https://sociabuzz.com/studiofru/donate" 
-          target="_blank">
-            <RecommendIcon sx={{mr:1}} />
-            Beri Dukungan
-        </Button>       
+    <div className="min-h-screen">     
           {children}
     </div>
     <Footer />
