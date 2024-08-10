7485a79798b7e6545e656e9aa7a8af06bcd31187
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-08-10 22:17:55 +0700

    91th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -315,7 +315,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                                 
+    </url>   
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/khasiat-sambiloto-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                   
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
