6e33c6a8087e5ea658a7592b7d995a54223b1c34
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-08-17 10:46:42 +0700

    93rd commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -369,7 +369,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                                               
+    </url>    
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/pantai-tirtamaya-indramayu</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                                 
     ${postsEn.map(({ slug, updatedAt }) => `
     <url>
         <loc>https://studiofrugreenproject.com/en/posts/${slug}</loc>
