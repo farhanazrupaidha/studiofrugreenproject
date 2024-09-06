476de2e394b6e9cfe8769105cebf100155eb5e57
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-09-06 09:23:40 +0700

    98th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -446,7 +446,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                                                                   
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/tanaman-budidaya-tertua-di-dunia-ada-di-indonesia</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                                                          
 </urlset>
 `;
 
