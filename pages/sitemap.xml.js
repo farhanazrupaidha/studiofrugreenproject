d8812412b2cb884da88e3a16eb4257189d9a5956
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-09-11 15:28:58 +0700

    98th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -452,7 +452,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                                                                          
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/penggunaan-hanjuang-sebagai-tanaman-hias</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                                                              
 </urlset>
 `;
 
