85f6f4c96a9b461788155210a5b3e231679b339d
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-09-17 11:49:11 +0700

    100th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -56,7 +56,79 @@ export const getServerSideProps = async ({ res }) => {
         <priority>1.0</priority>
     </url>                                 
     `)  
-    .join('\n')}                                                                                                                                                    
+    .join('\n')}  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/fungsi-dan-manfaat-antioksidan-bagi-tubuh</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>   
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/pengertian-fungsi-dan-jenis-jenis-air</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/5-langkah-penanggulangan-dan-pencegahan-bencana-kekeringan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>    
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/sejarah-penyebaran-budidaya-padi</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>   
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/wisata-alam-danau-setu-patok-cirebon</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>        
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/penyebab-terjadinya-kekeringan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/tempat-wisata-wajib-dikunjungi-di-cirebon</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/tanaman-budidaya-tertua-di-dunia-ada-di-indonesia</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>        
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/penggunaan-hanjuang-sebagai-tanaman-hias</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>   
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/6-manfaat-kacang-hias-atau-kacang-pinto-bagi-kesehatan-tanah</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>  
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/tanaman-tanaman-yang-dapat-digunakan-untuk-mengatasi-inflamasi-pada-kulit</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url> 
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/tanaman-tanaman-yang-dapat-digunakan-untuk-mengatasi-inflamasi-pada-kulit</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                                                                                        
 </urlset>
 `;
 
