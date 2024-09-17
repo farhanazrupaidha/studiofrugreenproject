0c0d5c99eb538254f838335c135c1d49032b1c78
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-09-17 12:09:09 +0700

    100th commit

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -128,7 +128,13 @@ export const getServerSideProps = async ({ res }) => {
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>                                                                                                                                                                        
+    </url>   
+    <url>
+        <loc>https://studiofrugreenproject.com/posts/kegunaan-taka-sebagai-alternatif-bahan-pangan-dan-manfaatnya-bagi-kesehatan</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>                                                                                                                                                                           
 </urlset>
 `;
 
